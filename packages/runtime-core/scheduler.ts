e9454456805253730337345305034423c1804525
Cansiny0320 <1136843910@qq.com>
2021-09-11 21:43:01 +0800

    feat: scheduler

--- packages/runtime-core/scheduler.ts
+++ packages/runtime-core/scheduler.ts
@@ -1,4 +1,31 @@
-const p = Promise.resolve()
+const resolvedPromise = Promise.resolve()
+let isFlushing = false
+const queue: any[] = []
+
 export function nextTick(fn?: () => void): Promise<void> {
-  return fn ? p.then(fn) : p
+  return fn ? resolvedPromise.then(fn) : resolvedPromise
+}
+
+export function queueJob(job: any) {
+  if (!queue.includes(job)) {
+    queue.push(job)
+    queueFlush()
+  }
+}
+
+function queueFlush() {
+  if (!isFlushing) {
+    resolvedPromise.then(() => {
+      isFlushing = true
+      try {
+        // TODO: 这里用 for-each，queueJob while flushing 测试过不了，不知道为什么
+        for (const job of queue) {
+          job()
+        }
+      } finally {
+        queue.length = 0
+        isFlushing = false
+      }
+    })
+  }
 }
